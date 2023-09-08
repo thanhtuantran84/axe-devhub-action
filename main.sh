eddf8f0c9f82f1e18dc59af55df32dca1ffbc497
Gabe <41127686+Zidious@users.noreply.github.com>
2023-09-08 22:09:59 +0100

    feat: add support for a11y threshold settings (#104)

--- main.sh
+++ main.sh
@@ -27,26 +27,38 @@ Response=$(
     --url "$SERVER_URL/api-pub/v1/axe-watcher/gh/$COMMIT_SHA"
 )
 
-if [ "$ENABLE_A11Y_THRESHOLD" = "true" ]; then
-  IssueCount=$(echo "$Response" | jq .issues_over_a11y_threshold)
-else
-  IssueCount=$(echo "$Response" | jq .last_run_violation_count)
-fi
-
+IssueCount=$(echo "$Response" | jq .last_run_violation_count)
+IssuesOverA11yThreshold=$(echo "$Response" | jq .issues_over_a11y_threshold)
 AxeURL=$(echo "$Response" | jq -r .axe_url)
 ProjectName=$(echo "$Response" | jq -r .project_name)
-
-echo "Project: $ProjectName"
-echo "Axe URL: $SERVER_URL$AxeURL"
 
 # Only set outs when running in GitHub Actions.
 if [ -n "${GITHUB_OUTPUT+x}" ]; then
   echo "project=$ProjectName" >>"$GITHUB_OUTPUT"
   echo "axe_url=$SERVER_URL$AxeURL" >>"$GITHUB_OUTPUT"
   echo "issue_count=$IssueCount" >>"$GITHUB_OUTPUT"
+  echo "issues_over_a11y_threshold=$IssuesOverA11yThreshold" >>"$GITHUB_OUTPUT"
+fi
+
+echo "Project: $ProjectName"
+echo "axe DevHub found $IssueCount accessibility violations."
+
+if [ "$ENABLE_A11Y_THRESHOLD" = "true" ]; then 
+  echo "axe DevHub found $IssuesOverA11yThreshold accessibility violations over your a11y threshold."
+fi
+
+echo "See the full report on axe DevHub: $SERVER_URL$AxeURL"
+
+
+if [ "$ENABLE_A11Y_THRESHOLD" = "true" ]; then
+  if [ "$IssuesOverA11yThreshold" -gt 0 ]; then
+    exit 1
+  else
+    # If there are no issues over the threshold, we can exit successfully.
+    exit 0
+  fi
 fi
 
 if [ "$IssueCount" -gt 0 ]; then
-  echo "Found $IssueCount accessibility violations"
   exit 1
 fi
